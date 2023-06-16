8b927c946d1ab5b3d49c4bf02a8d889c58f30b14
shivvvank <shivvvank@gmail.com>
2023-06-16 21:50:43 +0530

    client changes, ssl addition

--- API/Program.cs
+++ API/Program.cs
@@ -11,11 +11,13 @@ builder.Services.AddDbContext<DataContext>(opt => //convention
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 var app = builder.Build();//Add services before builder
 
 //when we hit endpoint like weatherforecast it comes here 
 // Configure the HTTP request pipeline.
 // Is request made by authroised user?
 app.MapControllers(); //middleware to map controller endpoints
+app.UseCors(policyBuilder=>policyBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.Run();
