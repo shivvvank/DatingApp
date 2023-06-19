d8d944ac5218fd776e047992b76bd41411e8103c
shivvvank <shivvvank@gmail.com>
2023-06-20 01:37:11 +0530

    async pipe

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '_services/account.service';
+import { User } from 'models/User';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +12,25 @@ export class AppComponent implements OnInit {
   title = 'Dating app';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,private accountService: AccountService) {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
+    this.getUsers();
+    this.setCurrentUser();
   }
 
-
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe({ // get returns observable and then we subscribe to it 
+    next: response => this.users = response,
+    error: error => console.log(error),
+    complete: () => console.log('Request has completed')
+  })
+  }
+  setCurrentUser()
+  {
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user:User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
+  }
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '_services/account.service';
+import { User } from 'models/User';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
@@ -9,24 +11,23 @@ import { AccountService } from '_services/account.service';
 export class NavComponent implements OnInit {
 
   model : any = {};
-  loggedIn = false;
 
   constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
+
   login(){
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
       },
       error : error => console.log(error)
     });
   }
 
   logout(){
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 }
